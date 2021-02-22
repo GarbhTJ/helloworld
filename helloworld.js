f3ea4e1dfea7fcc0f42590fd7a32aa9aacb4fbe6
TJ <teej@TJ-PC.localdomain>
2021-02-22 16:37:52 +0000

    Change helloworld message

--- helloworld.js
+++ helloworld.js
@@ -7,7 +7,7 @@ http.createServer(function (request, response) {
    response.writeHead(200, {'Content-Type': 'text/plain'})
 
    // Send the response body as "Hello World from TJ!!!"
-   response.end('Hello World\n')
+   response.end('Goood Morninnnnnnng Wooooorld!!!!\n')
 }).listen(3000)
 
 // Console will print the message
